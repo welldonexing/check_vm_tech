596bf180991cba51fa5c0467cf52e8029da69da6
welldonexing <34572886+welldonexing@users.noreply.github.com>
2022-10-03 04:35:23 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
+apt install gcc
 yum install -y gcc gcc-c++ gdb
 wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.15.tar.gz
 tar zxvf virt-what-1.15.tar.gz
