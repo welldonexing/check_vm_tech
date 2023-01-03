83448b1302807cf371757df79ba98068a0fdf82d
welldonexing <34572886+welldonexing@users.noreply.github.com>
2023-01-04 01:40:36 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,8 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
 apt install gcc
 yum install -y gcc gcc-c++ gdb
-wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.15.tar.gz
-#wget https://github.com/welldonexing/check_vm_tech/raw/master/virt-what-1.15.tar.gz
+#wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.15.tar.gz
+wget https://raw.githubusercontent.com/welldonexing/check_vm_tech/master/virt-what-1.15.tar.gz
 tar zxvf virt-what-1.15.tar.gz
 cd virt-what-1.15/
 ./configure
